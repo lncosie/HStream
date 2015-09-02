649ab04dc35f269c761264a8e75dff720cebc25e
lncosie <lncosie@gmail.com>
2015-09-02 08:53:50 +0800

    init

--- note/src/main/java/com/lncosie/note/ActivityNote.java
+++ note/src/main/java/com/lncosie/note/ActivityNote.java
@@ -1,40 +1,128 @@
 package com.lncosie.note;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class ActivityNote extends Activity {
+import com.activeandroid.query.Select;
 
+import java.util.List;
+
+public class ActivityNote extends Activity {
+    ListView    listCategory;
+    ListView    listNotes;
+    NotesAdapter    notesAdapter;
+    CategoryAdapter categoryAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        listCategory=(ListView)findViewById(R.id.list_cateogry);
+        listNotes=(ListView)findViewById(R.id.list_notes);
+
         toolbar.inflateMenu(R.menu.menu_activity_note);
+        notesAdapter=new NotesAdapter(this);
+        categoryAdapter=new CategoryAdapter(this);
+        listCategory.setAdapter(categoryAdapter);
+        listNotes.setAdapter(notesAdapter);
+        listCategory.setOnItemClickListener(categoryClick);
+        listNotes.setOnItemClickListener(noteClick);
+        toolbar.setOnMenuItemClickListener(menu_click);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_note, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    Toolbar.OnMenuItemClickListener menu_click=new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            int id = item.getItemId();
+            switch (id)
+            {
+                case R.id.button_show_category:
+                    final boolean hide=listCategory.getX()>=0;
+                    final int width=listCategory.getWidth();
+                    listCategory.animate().x(hide ? -width : 0).withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            listNotes.setLayoutParams(new LinearLayout.LayoutParams(
+                                    0, listNotes.getHeight(), hide ? 1 : (float) 0.7));
+                        }
+                    }).start();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    break;
+                case R.id.button_note_add:
+                    notesAdapter.add(new Note("Only Test","MY"));
+                    break;
+                case R.id.button_note_delete:
+                    break;
+                case R.id.button_note_select:
+                    break;
+            }
+            return false;
+        }
+    };
+
+    AdapterView.OnItemClickListener categoryClick=new AdapterView.OnItemClickListener() {
+
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Category    category=categoryAdapter.getItem(position);
+            if(category.getCategory().equals("+"))
+            {
+                categoryAdapter.add(new Category("Cio.1"));
+                return;
+            }
+            notesAdapter.changeCategory(category);
+        }
+    };
+    AdapterView.OnItemClickListener noteClick=new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getApplication(),"cc",Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    static class    CategoryAdapter extends ArrayAdapter<Category>
+    {
+        public CategoryAdapter(Context context) {
+            super(context, R.layout.item_category,R.id.text_category);
+            add(new Category("+"));
+            List<Category>  categories=new Select().from(Category.class).execute();
+            addAll(categories);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return super.getView(position, convertView, parent);
+        }
+    }
+    static class    NotesAdapter extends ArrayAdapter<Note>
+    {
+        public NotesAdapter(Context context) {
+            super(context, R.layout.item_note, R.id.text_note_pure);
+
+        }
+        Category    category;
+        public void changeCategory(Category category)
+        {
+            this.category=category;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return super.getView(position, convertView, parent);
+        }
     }
 }
--- note/src/main/java/com/lncosie/note/system.kt
+++ note/src/main/java/com/lncosie/note/system.kt
@@ -1,40 +1,44 @@
 package com.lncosie.note
 
+import com.activeandroid.Model
+import com.activeandroid.annotation.Column
+import com.activeandroid.annotation.Table
+import com.activeandroid.query.Select
 import kotlin.properties.Delegates
 
-
-open class  Note(content:String?,pure:String?=null)
+@Table(name="NOTES")
+open class  Note(@Column(name="CATEGORY")var category:String?,@Column(name="CONTENT")val content:String?): Model()
 {
     var annex:List<Any>?=null;
-    var time:data by    Delegates.notNull()
+
+    override fun toString(): String = content?.toString()?:""
 }
-
-open class  Category(val category:String):Iterable<Note>
+@Table(name="CATEGORY")
+open class  Category(@Column(name="NAME")val category:String):Model(),Iterable<Note>
 {
-
+    init {
+        val categories = Select().from(javaClass<Note>()).where("category=?",category).execute<Note>()
+        notes.addAll(categories);
+    }
+    var notes:MutableCollection<Note> by Delegates.notNull();
     fun add(content:String?):Note
     {
-        val note=Note(content)
+        val note=Note(content,category)
         notes.add(note)
         return note;
     }
-    override fun iterator(): Iterator<Note> {
-        return notes.iterator();
-    }
-    var notes:MutableCollection<Note> by Delegates.notNull();
-
+    override fun iterator(): Iterator<Note> =notes.iterator()
+    override fun toString(): String = category;
 }
 
 open class FsNote:Iterable<Category>{
 
+    val categorys:MutableCollection<Category> by Delegates.notNull();
     fun add(category:String):Category
     {
         val first=categorys.firstOrNull { it.category.equals(category) }
         return first?.let { it }?:Category(category)
+    }
+    override fun iterator(): Iterator<Category> = categorys.iterator();
 
-    }
-    override fun iterator(): Iterator<Category> {
-        return categorys.iterator();
-    }
-    val categorys:MutableCollection<Category> by Delegates.notNull();
 }
